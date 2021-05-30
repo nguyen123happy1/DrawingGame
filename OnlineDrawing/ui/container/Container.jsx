ebf9d24c27817ca50f4426d827ec8a5792cd55dc
nguyen123happy1 <63740529+nguyen123happy1@users.noreply.github.com>
2021-05-31 01:10:42 +0700

    Create Container.jsx

---
+++ OnlineDrawing/ui/container/Container.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Board from '../board/Board';
+
+import './style.css';
+
+// Screen
+
+class Container extends React.Component
+{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            color: "#000000",
+            size: "5"
+        }
+    }
+
+    changeColor(params) {
+        this.setState({
+            color: params.target.value
+        })
+    }
+
+    changeSize(params) {
+        this.setState({
+            size: params.target.value
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                {/* USER'S SCREEN WHEN PLAYING */}
+                <div className="screen">
+                    {/* AREAS: USER-TOOLS, CHAT-BOX, BOARD */}
+                    <div className="areas">
+                        <div className="user">
+
+                        </div>
+                        {/* <div class="tools-section">
+                            <div className="color-picker-container">
+                                Select Brush Color : &nbsp; 
+                                <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
+                            </div>
+
+                            <div className="brushsize-container">
+                                Select Brush Size : &nbsp; 
+                                <select value={this.state.size} onChange={this.changeSize.bind(this)}>
+                                    <option> 5 </option>
+                                    <option> 10 </option>
+                                    <option> 15 </option>
+                                    <option> 20 </option>
+                                    <option> 25 </option>
+                                    <option> 30 </option>
+                                </select>
+                            </div>
+
+                        </div> */}
+                        {/* BOARD */}
+                        <div class="board-container">
+                            <Board color={this.state.color} size={this.state.size}></Board>
+                        </div>
+
+                        {/* Chat Box */}
+                        <div className="chat-box">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Container
