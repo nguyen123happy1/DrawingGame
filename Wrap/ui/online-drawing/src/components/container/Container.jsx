b462634714203735a3a464f64db6662b05eb2b9d
nguyen123happy1 <63740529+nguyen123happy1@users.noreply.github.com>
2021-06-18 00:42:38 +0700

    Background effect

--- Wrap/ui/online-drawing/src/components/container/Container.jsx
+++ Wrap/ui/online-drawing/src/components/container/Container.jsx
@@ -6,32 +6,17 @@ import './style.css';
 
 // Screen
 
-class Container extends React.Component
+const Container = () => 
 {
-    constructor(props) {
-        super(props);
+    const [color, setColor] = useState('black');
+    const [size, setSize] = useState("5");
 
-        this.state = {
-            color: "#000000",
-            size: "5"
-        }
-    }
+    useEffect(() => {
+        import('./index')
+      },[]);
 
-    changeColor(params) {
-        this.setState({
-            color: params.target.value
-        })
-    }
-
-    changeSize(params) {
-        this.setState({
-            size: params.target.value
-        })
-    }
-
-    render() {
-
-        return (
+    return (
+        <>
             <div className="container">
                 {/* USER'S SCREEN WHEN PLAYING */}
                 <div className="screen">
@@ -40,28 +25,26 @@ class Container extends React.Component
                         <div className="user">
 
                         </div>
-                        {/* <div class="tools-section">
-                            <div className="color-picker-container">
-                                Select Brush Color : &nbsp; 
-                                <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)}/>
-                            </div>
-
-                            <div className="brushsize-container">
-                                Select Brush Size : &nbsp; 
-                                <select value={this.state.size} onChange={this.changeSize.bind(this)}>
-                                    <option> 5 </option>
-                                    <option> 10 </option>
-                                    <option> 15 </option>
-                                    <option> 20 </option>
-                                    <option> 25 </option>
-                                    <option> 30 </option>
-                                </select>
-                            </div>
-
-                        </div> */}
+//                         <div class="tools-section">
+//                             <div className="color-picker-container">
+//                                 Select Brush Color : &nbsp; 
+//                                 <input type="color" value={color} onChange={e => setColor(e.target.value)}/>
+//                             </div>
+//                             <div className="brushsize-container">
+//                                 Select Brush Size : &nbsp; 
+//                                 <select value={size} onChange={e => setSize(e.target.value)}>
+//                                     <option> 5 </option>
+//                                     <option> 10 </option>
+//                                     <option> 15 </option>
+//                                     <option> 20 </option>
+//                                     <option> 25 </option>
+//                                     <option> 30 </option>
+//                                 </select>
+//                             </div>
+//                         </div>
                         {/* BOARD */}
                         <div class="board-container">
-                            <Board color={this.state.color} size={this.state.size}></Board>
+                            <Board color={color} size={size}></Board>
                         </div>
 
                         {/* Chat Box */}
@@ -70,9 +53,11 @@ class Container extends React.Component
                         </div>
                     </div>
                 </div>
+                <canvas id="canvas1"></canvas>
             </div>
-        )
-    }
+        </>
+    )
+
 }
 
 export default Container
